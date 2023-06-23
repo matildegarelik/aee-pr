1aeb6fb3b74c042a0b146c6f92754967ffef275b
matildegarelik <garelikmatilde@gmail.com>
2023-06-22 23:14:49 -0300

    logos y responsive

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // cambiar color de navbar en  scroll
 
 $(document).ready(function(){
-
     $(window).scroll(()=>{
         $('nav').toggleClass("scrolled", $(this).scrollTop()>300);
         $('nav').toggleClass("mt-5", $(this).scrollTop()<20);
-
+         if($(this).scrollTop()>300){
+            $('#logo').attr('src','images/Logo_aee_-01.png');
+        }else{
+            $('#logo').attr('src','images/Logo_aee_-03.png');
+        }
     })
 });
 
